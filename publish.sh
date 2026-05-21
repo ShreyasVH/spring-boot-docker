11d75efb9d28d8aa51ff449abb8bce124ddd6da4
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2026-05-21 21:53:20 +0530

    updated publish script to include both for macos and ubuntu (#9)

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
-VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
-docker build --build-arg VERSION=${VERSION} -t spring-boot-docker .
-docker tag spring-boot-docker shreyasvh/spring-boot-docker:${VERSION}
-docker tag spring-boot-docker shreyasvh/spring-boot-docker:latest
+IMAGE_NAME="shreyasvh/spring-boot-docker"
+VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec | tr -d '\r' | tail -n 1 | xargs)
+echo "IMAGE=[$IMAGE_NAME]"
+echo "VERSION=[$VERSION]"
+docker buildx create --use --name multiarch-builder 2>/dev/null || docker buildx use multiarch-builder
+docker buildx inspect --bootstrap
 docker login
-docker push shreyasvh/spring-boot-docker:${VERSION}
-docker push shreyasvh/spring-boot-docker:latest
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg VERSION="${VERSION}" -t "${IMAGE_NAME}:${VERSION}" -t "${IMAGE_NAME}:latest" --push .
